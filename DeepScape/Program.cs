fe23b0bd0aacb56049296ee3ec80e6630252cc3a
hunterashner <hunterashner@users.noreply.github.com>
2022-10-27 20:29:54 -0400

    Add project files.

---
+++ DeepScape/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using static Tensorflow.Binding;
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        var testing = tf.constant("Hello, TensorFlow!");
+        Console.WriteLine(testing);
+    }
+}
